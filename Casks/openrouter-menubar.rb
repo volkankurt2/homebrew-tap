ffd8ce22c7033b41c0ff7ce4735c979f41873d11
volkankurt2 <volkankurt2@gmail.com>
2026-02-28 23:00:57 +0300

    Add openrouter-menubar Cask

--- Casks/openrouter-menubar.rb
+++ Casks/openrouter-menubar.rb
@@ -2,19 +2,12 @@ cask "openrouter-menubar" do
   version "1.0.0"
   sha256 "0784fef93e4ee2ea0f1a9924ee50821aae399cc86cce0259333b827beba5d457"
 
-  url "https://github.com/volkankurt2/openrouter-menubar/releases/download/v#{version}/OpenRouter%20Balance-#{version}-arm64.dmg"
+  url "https://github.com/volkankurt2/openrouter-menubar/releases/download/v#{version}/OpenRouter.Balance-#{version}-arm64.dmg"
   name "OpenRouter Menubar"
   desc "A menubar app to monitor and switch OpenRouter API keys"
   homepage "https://github.com/volkankurt2/openrouter-menubar"
 
   app "OpenRouter Balance.app"
-
-  postflight do
-    system_command "xattr",
-                   args: ["-cr", "#{appdir}/OpenRouter Balance.app"],
-                   sudo: false,
-                   print_stderr: false
-  end
 
   zap trash: [
     "~/Library/Application Support/openrouter-menubar",
