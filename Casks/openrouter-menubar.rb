808e94965a2fb3c262accf2c418e292c6b58e9e6
volkankurt <volkankurt2@gmail.com>
2026-03-02 18:40:59 +0300

    chore: bump openrouter-menubar to 1.0.2

--- Casks/openrouter-menubar.rb
+++ Casks/openrouter-menubar.rb
@@ -1,6 +1,6 @@
 cask "openrouter-menubar" do
-  version "1.0.1"
-  sha256 "48403bba77bedee723b08330f2e43a49d9c7c3d3abfea31031b65424d37f97be"
+  version "1.0.2"
+  sha256 "1b84e34909a8bae28a96ddcf829f982ea334c78fef022bf1c9ee6d9099b04b54"
 
   url "https://github.com/volkankurt2/openrouter-menubar/releases/download/v#{version}/OpenRouter.Balance-#{version}-arm64.dmg"
   name "OpenRouter Menubar"
